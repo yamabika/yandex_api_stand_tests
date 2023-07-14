648cf644aefce5285db423030495375ea14c3116
Default User <default@example.com>
2023-07-15 03:04:31 +0700

    My first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://3b4dd53c-ddd4-498a-8f92-452f4d9ea13a.serverhub.praktikum-services.ru"
+CREATE_USER_PATH = "/api/v1/users/"
+GET_USER_TAB = "/api/db/resources/user_model.csv"
---
+++ data.py
@@ -0,0 +1,9 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Анатолий",
+    "phone": "+79995553322",
+    "address": "г. Москва, ул. Пушкина, д. 10"
+}
